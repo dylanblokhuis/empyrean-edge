74c1487470b207a902ecf0be44057e5ecf3c685a
dylanblokhuis <dylanblokhuis3@hotmail.com>
2021-12-01 23:20:12 +0100

    prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,6 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import db from "~/utils/db.server";
-
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
+import { MetaFunction, LoaderFunction, ActionFunction, useLoaderData, Form } from "remix";
+import db, { User } from "~/utils/db.server";
+import { v4 } from 'uuid'
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
@@ -12,11 +8,21 @@ type IndexData = {
 // https://remix.run/api/conventions#loader
 export let loader: LoaderFunction = async ({ request }) => {
   const users = await db.user.findMany();
+  return users
+};
 
-  console.log(users);
+export let action: ActionFunction = async () => {
+  const id = v4();
+  await db.user.create({
+    data: {
+      email: `${id}@example.org`,
+      password: "hello-world",
+      name: id
+    }
+  });
 
   return true
-};
+}
 
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
@@ -28,12 +34,24 @@ export let meta: MetaFunction = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
+  const data = useLoaderData<User[]>();
+
   return (
     <div className="remix__page">
       <main>
         <h2 className="text-red-600">Welcome to Remix!</h2>
 
-        <button>Logout</button>
+        Users:
+
+        <div>
+          {data.map(user => (
+            <p key={user.id}>{user.name}</p>
+          ))}
+        </div>
+
+        <Form method="post">
+          <button type="submit">Create new user!</button>
+        </Form>
 
       </main>
     </div>
